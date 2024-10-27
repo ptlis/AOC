d27df9a72bbb2a111b9e4c167b819944145e4727
ptlis <ptlis@ptlis.net>
2024-10-27 14:30:49 +0000

    Rework solution to AOC 2023 day 3 part 1 ready for part 2

--- 2023/03/src/main/kotlin/uk.org.fca.gearratios/Main.kt
+++ 2023/03/src/main/kotlin/uk.org.fca.gearratios/Main.kt
@@ -4,56 +4,62 @@ import java.io.File
 import kotlin.math.max
 import kotlin.math.min
 
+data class Coord(val x: Int, val y: Int)
+data class Symbol(val position: Coord, val symbol: Char)
+data class PartNumber(val startCoord: Coord, val number: Int, val symbol: Symbol)
+
 fun main () {
     val engine = File("data/engine")
         .useLines { it.toList() }
         .map { it.toCharArray() }
 
-    println(findPartNumbers(engine).sum())
-    println(findPartNumbers(engine).joinToString("\n"))
+    println(findPartNumbers(engine).sumOf { it.number })
 }
 
-fun findPartNumbers(engine: List<CharArray>): List<Int> {
-    var x = 0
-    var y = 0
-    val partNumbers = mutableListOf<Int>()
-
-    do {
-        do {
-            if (engine[y][x].isDigit()) {
-                val possiblePartNumber = getPossiblePartNumber(engine[y], x)
-                if (isPartNumber(engine, x, y, possiblePartNumber)) {
-                    partNumbers.add(possiblePartNumber.toInt())
-                    x = x + possiblePartNumber.length - 1
-                }
-            }
-
-            x++
-        } while (x < engine[y].size)
-
-        x = 0
-        y++
-    } while (y < engine.size)
-
-    return partNumbers.toList()
+fun findPartNumbers(engine: List<CharArray>): List<PartNumber> {
+    return findSymbols(engine).map { findPartNumbersBySymbol(engine, it) }.flatten()
 }
 
-fun getPossiblePartNumber(engineLine: CharArray, startIndex: Int): String {
-    var partNumber = charArrayOf()
-    for (x in startIndex..<engineLine.size) {
-        if (!engineLine[x].isDigit()) break
-        partNumber += engineLine[x]
+fun findPartNumber(engineLine: CharArray, symbol: Symbol, numberFoundPosition: Coord): PartNumber {
+    var startX = numberFoundPosition.x
+    var endX = numberFoundPosition.x
+
+    while (startX - 1 >= 0 && engineLine[startX - 1].isDigit()) {
+        startX--
     }
-    return partNumber.concatToString()
+
+    while (endX + 1 < engineLine.size && engineLine[endX + 1].isDigit()) {
+        endX++
+    }
+
+    return PartNumber(
+        Coord(startX, numberFoundPosition.y),
+        engineLine.slice(startX..endX).joinToString("").toInt(),
+        symbol
+    )
 }
 
-fun isPartNumber(engine: List<CharArray>, startX: Int, startY: Int, possiblePartNumber: String): Boolean {
-    for (y in max(0, startY - 1)..min(engine.size - 1, startY + 1)) {
-        for (x in max(0, startX - 1)..min(engine[y].size - 1, startX + possiblePartNumber.length)) {
-            if (!(engine[y][x].isDigit() || engine[y][x] == '.')) {
-                return true
+fun findPartNumbersBySymbol(engine: List<CharArray>, symbol: Symbol): List<PartNumber> {
+    val partNumbers: MutableList<PartNumber> = mutableListOf()
+    for (y in max(0, symbol.position.y - 1)..min(engine.size - 1, symbol.position.y + 1)) {
+        for (x in max(0, symbol.position.x - 1)..min(engine[y].size - 1, symbol.position.x + 1)) {
+            if (engine[y][x].isDigit()) {
+                partNumbers.add(findPartNumber(engine[y], symbol, Coord(x, y)))
             }
         }
     }
-    return false
+
+    return partNumbers.toSet().toList()
+}
+
+fun findSymbols(engine: List<CharArray>): List<Symbol> {
+    val symbols = mutableListOf<Symbol>()
+    for (y in engine.indices) {
+        for (x in engine[y].indices) {
+            if (!engine[y][x].isDigit() && engine[y][x] != '.') {
+                symbols.add(Symbol(Coord(x, y), engine[y][x]))
+            }
+        }
+    }
+    return symbols.toList()
 }
--- 2023/03/src/test/kotlin/uk.org.fca.gearratios/MainKtTest.kt
+++ 2023/03/src/test/kotlin/uk.org.fca.gearratios/MainKtTest.kt
@@ -1,5 +1,6 @@
 package uk.org.fca.gearratios
 
+import java.io.File
 import kotlin.test.assertEquals
 import kotlin.test.Test
 
@@ -18,45 +19,27 @@ class MainKtTest {
 """.trimIndent().lines().map { it.toCharArray() }
 
     @Test
-    fun testGetPossiblePartNumber() {
-        val input = "....7512.."
-        val expected = "7512"
-
-        assertEquals(expected, getPossiblePartNumber(input.toCharArray(), 4))
-    }
-
-    @Test
-    fun testIsPartNumber1() {
-        val expected = true
-
-        assertEquals(expected, isPartNumber(testEngine, 0, 0, "467"))
-    }
-
-    @Test
-    fun testIsPartNumber2() {
-        val expected = false
-
-        assertEquals(expected, isPartNumber(testEngine, 5, 0, "114"))
-    }
-
-    @Test
-    fun testIsPartNumber3() {
-        val expected = false
-
-        assertEquals(expected, isPartNumber(testEngine, 7, 5, "58"))
-    }
-
-    @Test
-    fun testIsPartNumber4() {
-        val expected = true
-
-        assertEquals(expected, isPartNumber(testEngine, 2, 6, "592"))
-    }
-
-    @Test
     fun testFindPartNumbers() {
-        val expected = listOf(467, 35, 633, 617, 592, 755, 664, 598)
+        val expected = listOf(
+            PartNumber(Coord(0, 0), 467, Symbol(Coord(3, 1), '*')),
+            PartNumber(Coord(2, 2), 35, Symbol(Coord(3, 1), '*')),
+            PartNumber(Coord(6, 2), 633, Symbol(Coord(6, 3), '#')),
+            PartNumber(Coord(0, 4), 617, Symbol(Coord(3, 4), '*')),
+            PartNumber(Coord(2, 6), 592, Symbol(Coord(5, 5), '+')),
+            PartNumber(Coord(1, 9), 664, Symbol(Coord(3, 8), '$')),
+            PartNumber(Coord(6, 7), 755, Symbol(Coord(5, 8), '*')),
+            PartNumber(Coord(5, 9), 598, Symbol(Coord(5, 8), '*')),
+        )
 
         assertEquals(expected, findPartNumbers(testEngine))
+    }
+
+    @Test
+    fun testSolution() {
+        val engine = File("data/engine")
+            .useLines { it.toList() }
+            .map { it.toCharArray() }
+
+        assertEquals(553079, findPartNumbers(engine).sumOf { it.number.toInt() })
     }
 }
