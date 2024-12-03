8c6c8b790ecae0654613d1419717a5bdc96a17b6
ptlis <ptlis@ptlis.net>
2024-12-03 15:22:42 +0000

    Refinements for AOC 2024 day 3 solutions

--- src/main/kotlin/uk.org.fca.adventofcode/y2024/day3/Day3.kt
+++ src/main/kotlin/uk.org.fca.adventofcode/y2024/day3/Day3.kt
@@ -1,66 +1,67 @@
 package uk.org.fca.adventofcode.y2024.day3
 
 import uk.org.fca.adventofcode.Day
-import uk.org.fca.adventofcode.y2024.day2.Day2.Companion.isLevelValid
 import java.math.BigInteger
 
 class Day3: Day() {
-    override fun part1Solution(): BigInteger {
-        return BigInteger.valueOf(solvePart1(dayData).toLong())
-    }
+    override fun part1Solution(): BigInteger =
+        BigInteger.valueOf(solvePart1(dayData).toLong())
 
-    override fun part2Solution(): BigInteger {
-        return BigInteger.valueOf(solvePart2(dayData).toLong())
-    }
+    override fun part2Solution(): BigInteger =
+        BigInteger.valueOf(solvePart2(dayData).toLong())
 
     override val day: Int get() = 3
     override val year: Int get() = 2024
 
     companion object {
-
         fun solvePart1(rawData: List<String>): Int =
-            extractMultiplyStatements(rawData).sumOf { it.first * it.second }
+            extractMultiplyStatements(rawData, true).sumOf { it.first * it.second }
 
         fun solvePart2(rawData: List<String>): Int =
-            extractMultiplyAndDoDontStatements(rawData).sumOf { it.first * it.second }
+            extractMultiplyStatements(rawData).sumOf { it.first * it.second }
 
+        private fun extractStatements(regex: Regex, rawData: List<String>): List<String> =
+            regex
+                .findAll(rawData.joinToString(""))
+                .map { it.groups.first()?.value!! }
+                .toList()
 
-        fun extractMultiplyStatements(rawData: List<String>): List<Pair<Int, Int>> {
-            val multiplyStatements = """mul\([0-9]{1,3},[0-9]{1,3}\)""".toRegex()
-                .findAll(rawData.joinToString("")).toList()
-                .map { it.groups.first()?.value }
-
-            return multiplyStatements
-                .map { it?.removePrefix("mul(")?.removeSuffix(")")?.split(',')?.map { it.toInt() } }
-                .map { Pair(it?.first()!!, it.last()) }
-        }
-
-        fun extractMultiplyAndDoDontStatements(rawData: List<String>): List<Pair<Int, Int>> {
-            val statements = """mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)""".toRegex()
-                .findAll(rawData.joinToString("")).toList()
-                .map { it.groups.first()?.value }
-
+        private fun extractMultiplyStatements(
+            rawData: List<String>,
+            ignoreDoDont: Boolean = false
+        ): List<Pair<Int, Int>> {
             var keep = true
 
-            val multiplyStatements = statements.filter {
-                var retval = false
-                when (it) {
-                    "do()" -> {
-                        keep = true
+            return parseMultiplyStatements(
+                extractStatements("""mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)""".toRegex(), rawData).filter {
+                    var retval = false
+                    if (ignoreDoDont) {
+                        retval = when (it) {
+                            "do()" -> false
+                            "don't()" -> false
+                            else -> true
+                        }
+                    } else {
+                        when (it) {
+                            "do()" -> {
+                                keep = true
+                            }
+                            "don't()" -> {
+                                keep = false
+                            }
+                            else -> {
+                                retval = true
+                            }
+                        }
                     }
-                    "don't()" -> {
-                        keep = false
-                    }
-                    else -> {
-                        retval = true
-                    }
+                    retval && keep
                 }
-                retval && keep
-            }
-
-            return multiplyStatements
-                .map { it?.removePrefix("mul(")?.removeSuffix(")")?.split(',')?.map { it.toInt() } }
-                .map { Pair(it?.first()!!, it.last()) }
+            )
         }
+
+        private fun parseMultiplyStatements(multiplyStatements: List<String>): List<Pair<Int, Int>> =
+            multiplyStatements
+                .map { "[0-9]{1,3}".toRegex().findAll(it).map { num -> num.value.toInt() } }
+                .map { Pair(it?.first()!!, it.last()) }
     }
 }
